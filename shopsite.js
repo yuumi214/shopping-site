9db71a0ed412c29d401e3b85288470849ea9ab7c
Yuumi Watanabe <yuumi214@gmail.com>
2017-09-27 05:07:55 +0000

    first commit

---
+++ shopsite.js
@@ -0,0 +1,12 @@
+//carousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
